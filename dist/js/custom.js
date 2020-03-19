3997f2f2818c55183476875b2856e75ad649b819
Bryan Johnson Araujo <xKbryan@users.noreply.github.com>
2020-03-19 17:22:57 -0300

    Header color, favicon, copyright, whatsapp API, fix images and texts

---
+++ dist/js/custom.js
@@ -0,0 +1,4 @@
+// Ano atual do direitos autorais
+var rodapeId = document.getElementById("rodape")
+var ano = new Date().getFullYear()
+rodapeId.innerHTML = `${ano} Devsoft. Todos direitos reservados`
